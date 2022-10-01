22e13fd9877d6d2b7b87d1a406109bd171240e69
Nilesh Mishra <55555595+nilesh9836@users.noreply.github.com>
2022-10-02 04:03:27 +0530

    add sorting on complete key code dialog's table
    add sorting on complete key code dialog's table

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ $(document).ready(function(e) {
   $.getJSON( "../keycode.json" , function( result ){
     keyCodeList = result;
     console.log({keyCodeList});
-    
+
     for (let counter = 0; counter <= keyCodeList.length - 1; counter++) {
       //const curIndx = document.getElementById("key-code-table-body").rows.length;
       const row = document.getElementById("key-code-table-body").insertRow();
@@ -172,3 +172,55 @@ function exportJSONToCSV(objArray) {
   element.click();
 }
 
+function table_sort() {
+	const styleSheet=document.createElement('style')
+	styleSheet.innerHTML=`
+        .order-inactive span {
+            visibility:hidden;
+        }
+        .order-inactive:hover span {
+            visibility:visible;
+        }
+        .order-active span {
+            visibility: visible;
+        }
+    `
+	document.head.appendChild(styleSheet)
+
+	document.querySelectorAll('th.order').forEach(th_elem => {
+		let asc=true
+		const span_elem=document.createElement('span')
+		span_elem.style="font-size:0.8rem; margin-left:0.5rem"
+		span_elem.innerHTML="▼"
+		th_elem.appendChild(span_elem)
+		th_elem.classList.add('order-inactive')
+
+		const index=Array.from(th_elem.parentNode.children).indexOf(th_elem)
+		th_elem.addEventListener('click', (e) => {
+			document.querySelectorAll('th.order').forEach(elem => {
+				elem.classList.remove('order-active')
+				elem.classList.add('order-inactive')
+			})
+			th_elem.classList.remove('order-inactive')
+			th_elem.classList.add('order-active')
+
+			if (!asc) {
+				th_elem.querySelector('span').innerHTML='▲'
+			} else {
+				th_elem.querySelector('span').innerHTML='▼'
+			}
+			const arr=Array.from(th_elem.closest("table").querySelectorAll('tbody tr'))
+			arr.sort((a, b) => {
+				const a_val=a.children[index].innerText
+				const b_val=b.children[index].innerText
+				return (asc)? a_val.localeCompare(b_val):b_val.localeCompare(a_val)
+			})
+			arr.forEach(elem => {
+				th_elem.closest("table").querySelector("tbody").appendChild(elem)
+			})
+			asc=!asc
+		})
+	})
+}
+
+table_sort()
